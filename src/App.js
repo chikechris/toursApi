137bfd4df1c5056b5e0addd532925cd1fce691d6
chikechris <cchike2017@gmail.com>
2020-11-15 07:02:30 -0500

    worked on Tours and Tour compponet

--- src/App.js
+++ src/App.js
@@ -3,16 +3,36 @@ import Loading from './Loading'
 import Tours from './Tours'
 
 const url = 'https://course-api.com/react-tours-project'
+
 function App() {
-  const [loading,  setLoading] = useState(false) 
+  const [loading,  setLoading] = useState(true) 
   const [tours, setTours] = useState([]) 
+
+  const fetchTours = async () => {
+    setLoading(true) 
+
+    try {
+       const res = await fetch(url) 
+       const tours = await res.json() 
+       setLoading(false) 
+       setTours(tours)
+    } catch (error) {
+      setLoading(false) 
+      console.log(error)
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, [])
+  
   if(loading) {
     return <main>
       <Loading/>
     </main>
   }
   return <main>
-    <Tours/>
+    <Tours tours={tours}/>
   </main>
 }
 
